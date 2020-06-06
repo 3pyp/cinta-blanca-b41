3f4c347737058721fb799bada929ba3912003bc7
Cristian Gutierrez <cgutierez@hugeinc.com>
2020-06-05 21:15:32 -0500

    Primera clase de Javascript agregada

---
+++ Clase-Javascript-inicio/index.js
@@ -0,0 +1,28 @@
+// VARIABLES
+
+// String (cadena de texto)
+var limon = 'un limon';
+
+// Number
+var azucar = 20; 
+
+// Booleanos (solo puede tener verdadero(true) o Falso (false))
+var tieneDulce = false;
+
+ // Arreglos
+var instrumentos = ['Cuchillo', 'Tabla para cortar', 'vaso', 'cuchara', 'Exprimidor', 20, [], {}];
+
+// console.log(instrumentos[2])
+
+// Objetos
+var limonero = {
+  nombre: 'Cristian', // Atributo o propiedad
+  edad: 25, // Atributo o propiedad
+  estatura: '165cm', // Atributo o propiedad
+}
+
+console.log(limonero.nombre + ' tiene una estatura de ' + limonero.estatura) // Concatenar Strings (unir cadenas de textos)
+
+var nombre = prompt('Escribe tu nombre')
+
+console.log("Hola " + nombre)
