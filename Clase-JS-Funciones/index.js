9e46d3c15bfaef3413bc879e8d41f453a3b29f33
Cristian Gutierrez <cgutierez@hugeinc.com>
2020-06-09 19:46:42 -0500

    Primeras funciones agregadas

---
+++ Clase-JS-Funciones/index.js
@@ -0,0 +1,26 @@
+function suma(numero1, numero2){
+  console.log(numero1 + numero2)
+}
+
+suma(10, 5)
+suma(20, 15)
+
+function obtenerPastel (dinero){
+  if(dinero === 10) {
+    console.log("Pastel obtenido")
+  } else {
+    console.log("Por favor ingresa tu dinero")
+  }
+}
+
+obtenerPastel(10)
+
+function mostrarNombre(nombre){
+  console.log(nombre)
+}
+
+mostrarNombre('Cristian')
+
+mostrarNombre('Felipe')
+
+mostrarNombre('Carlos')
