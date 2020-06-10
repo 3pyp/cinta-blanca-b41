c896a0fa5d712cd60ea7e1ba8d73e7dc6d81ca58
Cristian Gutierrez <cgutierez@hugeinc.com>
2020-06-09 20:03:32 -0500

    Ejercicio 1 agregado

--- Clase-JS-Funciones/index.js
+++ Clase-JS-Funciones/index.js
@@ -2,8 +2,8 @@ function suma(numero1, numero2){
   console.log(numero1 + numero2)
 }
 
-suma(10, 5)
-suma(20, 15)
+// suma(10, 5)
+// suma(20, 15)
 
 function obtenerPastel (dinero){
   if(dinero === 10) {
@@ -13,14 +13,25 @@ function obtenerPastel (dinero){
   }
 }
 
-obtenerPastel(10)
+// obtenerPastel(10)
 
 function mostrarNombre(nombre){
   console.log(nombre)
 }
 
-mostrarNombre('Cristian')
+// mostrarNombre('Cristian')
 
-mostrarNombre('Felipe')
+// mostrarNombre('Felipe')
 
-mostrarNombre('Carlos')
+// mostrarNombre('Carlos')
+
+function mayorDeEdad(edad){
+  if(edad >= 18){
+    console.log("Eres mayor de edad")
+  } else {
+    console.log("No eres mayor de edad")
+  }
+}
+
+mayorDeEdad(15)
+
