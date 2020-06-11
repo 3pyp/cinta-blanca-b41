875eb4d4656657fe0e5a11aa920897732af33a68
Cristian Gutierrez <cgutierez@hugeinc.com>
2020-06-10 20:17:05 -0500

    Uniendo Javascript con html

---
+++ Clase-JS-Objetos-HTML/index.js
@@ -0,0 +1,34 @@
+// OBJETOS
+var mesa = {
+  altura: "90cm",
+  ancho: "70cm",
+  color: "cafe",
+  patas: 4,
+  sillas: ["silla1", "silla2", "silla3", "silla4"],
+  marca: {
+    nombre: "Rimax",
+    vendedor: "Andres",
+  },
+  aLaVenta: true,
+
+  expandir: function () {
+    return "Se expandio";
+  },
+};
+
+mesa.altura = "75cm";
+
+// console.log(mesa.color)
+
+console.log(mesa.expandir())
+
+// HTML y Javascript
+
+var titulo = document.getElementById("titulo"); // Obtenemos el H1 de nuestro HTML con el id titulo
+var btn = document.getElementById("boton");  // Obtenemos el button de nuestro HTML con el id boton
+
+function cambiarTitulo (){
+  titulo.innerHTML = "Se cambio el texto";
+}
+
+btn.addEventListener("click", cambiarTitulo)
